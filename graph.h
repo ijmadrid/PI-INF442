53b93a269411700ed6804c689e67a653019f9d84
Ignacio <ijmadrid@uc.cl>
2017-05-25 19:55:47 +0200

    25 mai

--- graph.h
+++ graph.h
@@ -9,19 +9,19 @@ class Graph{
 		int n;	// nb de noeuds
 		list<int> *adj;  // liste d'adjacence (plus performante qu'avec une matrice d'adj.)
 		list<int> *scc;  // liste de Strongly Connected Components
+
+		// constructor : graphe vide de taille nb_noeuds
+		Graph(int)
+
+		// constructor : build graph from a text file
+		Graph(const char *);
 }
 
-// constructor : graphe vide de taille nb_noeuds
-Graph::Graph(int)
-
-// constructor : build graph from a text file
-Graph::Graph(const char *);
-
 // add the edge i -> j
-void Graph::addEdge(int, int);
+void addEdge(int, int);
 
 // Kosajaru's algorithm
-void Graph::kosajaru();
+void kosajaru();
 
 
 #endif
